37f96877a87f500d3145bdec99939bcb96368e6c
beh <beh.ehap@gmail.com>
2024-10-21 13:03:28 +0330

    change: moved the divs and the renderContent function out of the menu in the profile file.

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -9,8 +9,7 @@ import Notifications from "./Notifications";
 import MyTickets from "./MyTickets";
 import CompleteProfileModal from "../components/CompleteProfileModal"; // Your modal
 
-function SidebarMenu() {
-  const [activeMenu, setActiveMenu] = useState("profile");
+function SidebarMenu({ activeMenu, setActiveMenu }) {
   const [isProfileComplete, setIsCompleteProfile] = useState(true);
   const [showCompleteProfileModal, setShowCompleteProfileModal] =
     useState(false);
@@ -57,21 +56,6 @@ function SidebarMenu() {
     }
   };
 
-  const renderContent = () => {
-    switch (activeMenu) {
-      case "profile":
-        return <EditProfile />;
-      case "notifications":
-        return <Notifications />;
-      case "tickets":
-        return <MyTickets />;
-      case "logout":
-        return <div>Logging out...</div>;
-      default:
-        return <div>Select a menu item to see content</div>;
-    }
-  };
-
   function MenuItem({ icon, label, isActive = false, onClick }) {
     return (
       <div
@@ -89,7 +73,7 @@ function SidebarMenu() {
   }
 
   return (
-    <div className="flex rounded-lg flex-col p-5 bg-primary-500 md:flex-row md:m-5 md:bg-white w-full">
+    <div className="my-5">
       {/* Profile Completion Modal */}
       <CompleteProfileModal
         showCompleteProfileModal={showCompleteProfileModal}
@@ -97,95 +81,89 @@ function SidebarMenu() {
       />
 
       {/* Top Navbar for mobile, Sidebar for desktop */}
-      <div className="w-full rounded-lg bg-white md:w-64 transition-all duration-300 relative">
-        <div className="p-2 shadow-lg">
-          {/* Profile Section (Avatar only shown on desktop) */}
-          <div className="hidden md:flex flex-col items-center mt-16">
-            <Avatar className="mb-4">
-              <AvatarImage
-                src={
-                  userData && userData.image
-                    ? userData.image
-                    : "https://via.placeholder.com/150"
-                }
-                alt="Profile"
-              />
-            </Avatar>
-            {/* Conditionally render user name if userData is available */}
-            {userData ? (
-              <>
-                <h3 className="text-lg font-medium text-gray-700">
-                  {userData.name}
-                </h3>
 
-                {/* <p className="text-sm text-gray-500">{userData.username}</p>
+      <div className="p-2 bg-white rounded-lg shadow-lg">
+        {/* Profile Section (Avatar only shown on desktop) */}
+        <div className="hidden md:flex flex-col items-center mt-16">
+          <Avatar className="mb-4">
+            <AvatarImage
+              src={
+                userData && userData.image
+                  ? userData.image
+                  : "https://via.placeholder.com/150"
+              }
+              alt="Profile"
+            />
+          </Avatar>
+          {/* Conditionally render user name if userData is available */}
+          {userData ? (
+            <>
+              <h3 className="text-lg font-medium text-gray-700">
+                {userData.name}
+              </h3>
+
+              {/* <p className="text-sm text-gray-500">{userData.username}</p>
                 <p className="text-sm text-gray-500">{userData.phone}</p> */}
-              </>
-            ) : (
-              <h3 className="text-lg font-medium text-gray-700">Loading...</h3>
-            )}
-          </div>
-
-          {/* Mobile/Tablet Menu (Flex row on small screens, vertical on desktop) */}
-
-          <div className="flex justify-around md:flex-col md:space-y-6 ">
-            {/* Decorative line */}
-
-            <MenuItem
-              icon={
-                <IoPersonCircleOutline
-                  className={`${
-                    activeMenu === "profile" ? "text-accent-600" : ""
-                  } transition-all duration-300 rounded-lg w-10 h-10`}
-                />
-              }
-              label="ویرایش پروفایل"
-              onClick={() => handleMenuClick("profile")}
-              isActive={activeMenu === "profile"}
-            />
-            <MenuItem
-              icon={
-                <FaBell
-                  className={`${
-                    activeMenu === "notifications" ? "text-secondary-500" : ""
-                  } transition-all duration-300 rounded-lg w-7 h-7`}
-                />
-              }
-              label="اعلان ها"
-              onClick={() => handleMenuClick("notifications")}
-              isActive={activeMenu === "notifications"}
-            />
-            <MenuItem
-              icon={
-                <HiTicket
-                  className={`${
-                    activeMenu === "tickets" ? "text-primary-700" : ""
-                  } transition-all duration-300 rounded-lg w-10 h-10`}
-                />
-              }
-              label="تیکت ها"
-              onClick={() => handleMenuClick("tickets")}
-              isActive={activeMenu === "tickets"}
-            />
-            <MenuItem
-              icon={
-                <CiLogout
-                  className={`${
-                    activeMenu === "logout" ? "text-red-500" : ""
-                  } transition-all duration-300 rounded-lg w-10 h-10`}
-                />
-              }
-              label="خروج از حساب کاربری"
-              onClick={() => handleMenuClick("logout")}
-              isActive={activeMenu === "logout"}
-            />
-          </div>
+            </>
+          ) : (
+            <h3 className="text-lg font-medium text-gray-700">Loading...</h3>
+          )}
         </div>
-      </div>
 
-      {/* Main Content Area */}
-      <div className="flex justify-center flex-grow w-full pt-5 ">
-        {renderContent()} {/* Render content based on active menu */}
+        {/* Mobile/Tablet Menu (Flex row on small screens, vertical on desktop) */}
+
+        <div className="flex justify-around md:flex-col md:space-y-6 ">
+          {/* Decorative line */}
+
+          <MenuItem
+            icon={
+              <IoPersonCircleOutline
+                className={`${
+                  activeMenu === "profile" ? "text-accent-600" : ""
+                } transition-all duration-300 rounded-lg w-10 h-10`}
+              />
+            }
+            label="ویرایش پروفایل"
+            onClick={() => handleMenuClick("profile")}
+            isActive={activeMenu === "profile"}
+          />
+          <MenuItem
+            icon={
+              <FaBell
+                className={`${
+                  activeMenu === "notifications" ? "text-secondary-500" : ""
+                } transition-all duration-300 rounded-lg w-7 h-7`}
+              />
+            }
+            label="اعلان ها"
+            onClick={() => handleMenuClick("notifications")}
+            isActive={activeMenu === "notifications"}
+          />
+          <MenuItem
+            icon={
+              <HiTicket
+                className={`${
+                  activeMenu === "tickets" ? "text-primary-700" : ""
+                } transition-all duration-300 rounded-lg w-10 h-10`}
+              />
+            }
+            label="تیکت ها"
+            onClick={() => handleMenuClick("tickets")}
+            isActive={activeMenu === "tickets"}
+          />
+          <MenuItem
+            icon={
+              <CiLogout
+                className={`${
+                  activeMenu === "logout" ? "text-red-500" : ""
+                } transition-all duration-300 rounded-lg w-10 h-10`}
+              />
+            }
+            label="خروج از حساب کاربری"
+            onClick={() => handleMenuClick("logout")}
+            isActive={activeMenu === "logout"}
+          />
+        </div>
       </div>
     </div>
   );
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import Navbar from "@/components/Navbar"; // Assuming you have a Navbar
 import EditProfile from "./EditProfile";
 import Notifications from "./Notifications";
@@ -8,9 +9,28 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Menu from "./Menu";
 
 function Profile() {
+  const [activeMenu, setActiveMenu] = useState("profile");
+
+  const renderContent = () => {
+    switch (activeMenu) {
+      case "profile":
+        return <EditProfile />;
+      case "notifications":
+        return <Notifications />;
+      case "tickets":
+        return <MyTickets />;
+      case "logout":
+        return <div>Logging out...</div>;
+      default:
+        return <div>Select a menu item to see content</div>;
+    }
+  };
   return (
-    <div>
-      <Menu />
+    <div className="flex justify-items- rounded-lg flex-col p-5 bg-primary-500 md:flex-row md:m-5 md:bg-white ">
+      <Menu activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
+      <div className="flex w-full justify-center items-center flex-grow  pt-5 ">
+        {renderContent()} {/* Render content based on active menu */}
+      </div>
     </div>
   );
 }
