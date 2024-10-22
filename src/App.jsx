7e0c8d148a62e287a615d1364118f180b41d9bb8
beh <beh.ehap@gmail.com>
2024-10-22 19:06:33 +0330

    deleted extralines of code (extra import)

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Home from "./pages/Home.jsx";
 import Profile from "./pages/Profile.jsx";
 import Footer from "./components/Footer1.jsx";
 import Navbar from "./components/Navbar.jsx";
-import CreateTicket from "./pages/CreateTicket.jsx/index.js";
-import TicketChat from "./pages/TicketChat.jsx";
+
 import CompleteProfileModal from "@/components/CompleteProfileModal";
 
 function App() {
